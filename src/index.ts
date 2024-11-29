b9f370b8290e45e568f2f8b2e5168f96f14e37ec
Sunil Pai <spai@cloudflare.com>
2024-11-29 10:43:38 +0000

    add a databse init and a stub for an alarm

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,33 @@ type Env = {
 };
 
 export class Session extends Server<Env> {
+  constructor(state: DurableObjectState, env: Env) {
+    super(state, env);
+
+    // setup a messages sql table
+    // id, message, step, context
+    this.ctx.storage.sql.exec(`
+      CREATE TABLE IF NOT EXISTS messages (
+        id TEXT PRIMARY KEY,
+        message TEXT,
+        step TEXT,
+        context TEXT
+      )
+    `);
+
+    this.ctx.blockConcurrencyWhile(async () => {
+      // wait for 10 seconds
+      if (!this.ctx.storage.getAlarm()) {
+        // this.ctx.storage.setAlarm();
+        // find the time for sunday night
+      }
+    });
+  }
+
+  onAlarm() {
+    // you will drain to S3 or whatever
+  }
+
   onConnect(connection: Connection<unknown>) {
     console.log("connected", this.name);
     connection.send(
@@ -18,6 +45,9 @@ export class Session extends Server<Env> {
   }
   onMessage(connection: Connection<unknown>, message: WSMessage) {
     console.log("message", message);
+  }
+  onRequest(request: Request) {
+    return new Response("Hello, World!");
   }
 }
 
